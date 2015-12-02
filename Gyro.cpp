f349eacdfcd5128aa6f75d8251e690145f9bf937
hejohnson <hejohnson@wpi.edu>
2015-12-01 21:34:33 -0500

    Switched to reading x-axis only, for mounting directly on arduino

--- Gyro.cpp
+++ Gyro.cpp
@@ -1,7 +1,3 @@
-//Written by Joe St. Germain 8/1/15
-// Simply keeps track of the gyro reading from an minimu9
-// I reccommend going one step further with a 
-//complimentary filter with the accelerometer for greater accuracy over longer periods of time. 
 #include "Gyro.h"
 
 Gyro::Gyro() {
@@ -10,64 +6,50 @@ Gyro::Gyro() {
 
 void Gyro::initialize() {
   Wire.begin(); // i2c begin
+  if (!gyro.init()) // gyro init
+  {
+    Serial.println("Failed to autodetect gyro type!");
+    while (1); 
+  }
   gyro.enableDefault(); // gyro init. default 250/deg/s
+
   delay(1000);// allow time for gyro to settle
-  gdriftz = 0;
-  gyro_zcumulative = 0;
-  gerrz = 0;
+  gdriftx = 0;
+  gyro_xcumulative = 0;
+  gerrx = 0;
   setGyroOffset();
   setGyroDrift();
   prevTime = millis();
 }
 
-float Gyro::getXGyro() {
+float Gyro::getGyro() {
   return gyro_xcumulative;
-}
-
-float Gyro::getYGyro() {
-  return gyro_ycumulative;
-}
-
-float Gyro::getZGyro() {
-  return gyro_zcumulative;
 }
 
 void Gyro::updateGyro() {
   Dt = (float)(millis()-prevTime)/1000.0;
   gyro.read(); // read gyro
 
-  //gyro_x=(float)(gyro.g.x-gerrx)*G_gain; // offset by error then multiply by gyro gain factor 
-  //gyro_y=(float)(gyro.g.y-gerry)*G_gain;
-  gyro_z=(float)(gyro.g.z-gerrz)*G_gain;
-
-  //gyro_x = (gyro_x-gdriftx)*Dt; // Multiply the angular rate by the time interval
-  //gyro_y = (gyro_y-gdrifty)*Dt; 
-  gyro_z = (gyro_z)*Dt;
-  //gyro_z -= gdriftz*Dt;
-
-  //gyro_xcumulative += gyro_x; // add the displacment(rotation) to the cumulative displacment
-  //gyro_ycumulative += gyro_y;
-  gyro_zcumulative += gyro_z;
+  gyro_x=(float)(gyro.g.x-gerrx)*G_gain;
+ 
+  gyro_x = (gyro_x)*Dt;
+  gyro_x -= gdriftx*Dt;
+ 
+  gyro_xcumulative += gyro_x;
 
   prevTime=millis(); //reset prevTime
 }
 
 void Gyro::setGyroOffset() {
-  for(int i =0;i<100;i++){  // takes 100 samples of the gyro
+  for(int i=0;i<100;i++){  // takes 100 samples of the gyro
     gyro.read();
     //gerrx+=gyro.g.x;
     //gerry+=gyro.g.y;
-    gerrz+=gyro.g.z;
+    gerrx+=gyro.g.x;
     delay(25);
   }
 
-  //gerrx = gerrx/100; // average reading to obtain an error/offset
-  //gerry = gerry/100;
-  gerrz = gerrz/100;
-
-  // Serial.println(gerrx); // print error vals
-  // Serial.println(gerry);  
-  // Serial.println(gerrz);
+  gerrx = gerrx/100;
 }
 
 void Gyro::setGyroDrift() {
@@ -79,13 +61,9 @@ void Gyro::setGyroDrift() {
     delay(25);
   }
   long etime = millis();
-  //gdriftx = gdriftx/(etime-stime)*1000; // Accumulated rate over the sample time to obtain drift rate
-  //gdrifty = gdrifty/(etime-stime)*1000;
-  gdriftz = gyro_zcumulative/(etime-stime)*1000;
-  Serial.println(gyro_zcumulative);
-  gyro_zcumulative = 0;
-  Serial.println(gdriftz);
-  // Serial.println(gdriftx, 5); // print error vals
-  // Serial.println(gdrifty, 5);  
-  // Serial.println(gdriftz, 5);
+
+  gdriftx = gyro_xcumulative/(etime-stime)*1000;
+  Serial.println(gyro_xcumulative);
+  gyro_xcumulative = 0;
+  Serial.println(gdriftx);
 }
--- Gyro.h
+++ Gyro.h
@@ -8,16 +8,14 @@ class Gyro {
 	public: 
 		Gyro ();
 		void initialize();
-		float getXGyro();
-		float getYGyro();
-		float getZGyro();
+		float getGyro();
 		void updateGyro();
 		void setGyroOffset();
 		void setGyroDrift();
 	private:
 		long prevTime;   //general purpose timer
 		float G_gain=.00875; // gyros gain factor for 250deg/sec
-		float gyro_x, gyro_y, gyro_z, gyro_xcumulative, gyro_ycumulative, gyro_zcumulative, gerrx, gerry, gerrz, gdriftx, gdrifty, gdriftz, Dt;
+		float gyro_x, gyro_xcumulative, gerrx, gdriftx, Dt;
 		L3G gyro;
 };
 #endif
